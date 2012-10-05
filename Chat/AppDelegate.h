2f52f65171bc2120e0be04b74257b4cc969c86f1
jarinosuke <naokiishikawa0808@gmail.com>
2012-10-05 11:57:16 +0900

    Initial Commit

---
+++ Chat/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Chat
+//
+//  Created by jarinosuke on 10/5/12.
+//  Copyright (c) 2012 jarinosuke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Chat/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Chat
+//
+//  Created by jarinosuke on 10/5/12.
+//  Copyright (c) 2012 jarinosuke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
